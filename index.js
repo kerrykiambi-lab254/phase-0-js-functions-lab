0fdf4b78b2c14cef98f1f27dc4fa9a32793ea632
kerrykiambi-lab254 <kerry.kiambi@student.moringaschool.com>
2026-03-28 03:15:07 +0000

    first

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+// index.js
 
+// Calculate tax: tests call calculateTax(amount) and expect 10% of amount
+function calculateTax(amount) {
+  const taxRate = 0.10; // fixed 10% tax
+  return amount * taxRate;
+}
 
+// Convert string to uppercase
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
 
+// Find maximum of two numbers (tests only pass two arguments)
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
 
+// Check if a string is a palindrome
+function isPalindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// Calculate discounted price: tests call calculateDiscountedPrice(originalPrice, discountPercentage)
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  // discountPercentage is given as a whole number (e.g., 20 for 20%)
+  const discountRate = discountPercentage / 100;
+  const discountedPrice = originalPrice * (1 - discountRate);
+  return discountedPrice;
+}
+
+// Export all functions
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
+
